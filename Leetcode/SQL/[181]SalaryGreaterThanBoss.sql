07170ec655b9c158fb8b9c696f0a69f031f12efc
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-05-24 16:28:17 +0600

    Update and rename 181.sql to [181]SalaryGreaterThanBoss.sql

--- Leetcode/SQL/[181]SalaryGreaterThanBoss.sql
+++ Leetcode/SQL/[181]SalaryGreaterThanBoss.sql
@@ -1,3 +1,5 @@
+# 181. Employees Earning More Than Their Managers
+
 SELECT e1.Name AS Employee FROM Employee AS e1 
 LEFT OUTER JOIN Employee AS e2 ON e1.ManagerId = e2.Id 
 WHERE e1.Salary > e2.Salary
