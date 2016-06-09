e1700129f4d5df1ffaeb1fb0cb50928c69c9aae5
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-06-09 17:15:09 +0600

    Create [38]-Count-and-say.cs

---
+++ Leetcode/Algorithms/[38]-Count-and-say.cs
@@ -0,0 +1,31 @@
+        public string CountAndSay(int n)
+        {
+            int cnt = 0;
+            var numList = new List<string> { "1", "11" };
+            for (int i = 2; i < n; i++)
+            {
+                var prevNumber = numList[i - 1];
+                var prevDigit = prevNumber[0];
+                var newNum = String.Empty;
+
+                for (var j = 1; j <= prevNumber.Length; j++)
+                {
+                    var currentDigit = j<prevNumber.Length ? prevNumber[j] : '\0';
+
+                    if (currentDigit == prevDigit)
+                    {
+                        cnt++;
+                    }
+                    else
+                    {
+                        cnt++;
+                        newNum += cnt.ToString() + prevDigit;
+                        cnt = 0;
+                        prevDigit = currentDigit;
+                    }
+                }
+                numList.Add(newNum);
+
+            }
+            return numList[n - 1].ToString();
+        }
