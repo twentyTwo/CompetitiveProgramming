d828b8697fcdf1dd2e726aee1ff6f198e77e0ce2
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-06-12 12:31:13 +0600

    Create [263]-Ugly-numbers.cs

---
+++ Leetcode/Algorithms/[263]-Ugly-numbers.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool IsUgly(int num) {
+    if(num<=0) return false;
+    while (true)
+        {
+            if (num == 1) return true;
+            if (num%2 == 0) num /= 2;
+            else if (num%3 == 0) num /= 3;
+            else if (num%5 == 0) num /= 5;
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
