eb98ce87dbab4ca300a5922c15362682f096334a
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-06-19 17:38:42 +0600

    Create [36]-Valid-sudoku.cs

---
+++ Leetcode/Algorithms/[36]-Valid-sudoku.cs
@@ -0,0 +1,93 @@
+public class Solution {
+    public bool IsValidSudoku(char[,] board)
+        {
+            var nums = new int[10];
+
+            /*Check whether same number exists in a row */
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    if (board[i, j] != '.')
+                    {
+                        var num = board[i, j];
+                        if (num != '.')
+                        {
+                            var index = int.Parse(num.ToString());
+                            if (nums[index] != 0)
+                            {
+                                return false;
+                            }
+                            nums[index] = 1;
+                        }
+                    }
+                }
+                // Re-initialize the number checker array nums to 0
+                for (var k = 0; k < 10; k++)
+                {
+                    nums[k] = 0;
+                }
+            }
+            
+            /*Check whether same number exists in a column */
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    if (board[j, i] != '.')
+                    {
+                        var num = board[j, i];
+                        if (num != '.')
+                        {
+                            var index = int.Parse(num.ToString());
+                            if (nums[index] != 0)
+                            {
+                                return false;
+                            }
+                            nums[index] = 1;
+                        }
+                    }
+                }
+                // Re-initialize the number checker array nums to 0
+                for (var k = 0; k < 10; k++)
+                {
+                    nums[k] = 0;
+                }
+            }
+
+            /* Check whether the 9 blocks of 3*3 contains same number */
+            for (var R = 0; R < 7; R = R + 3)
+            {
+                for (var C = 0; C < 7; C = C + 3)
+                {
+                    for (var i = R; i <= 2 + R; i++)
+                    {
+                        for (var j = C; j <= 2 + C; j++)
+                        {
+                            var num = board[j, i];
+                            if (num != '.')
+                            {
+                                var index = int.Parse(num.ToString());
+                                if (nums[index] != 0)
+                                {
+                                    return false;
+                                }
+                                nums[index] = 1;
+                            }
+                        }
+                    }
+                    // Re-initialize the number checker array nums to 0
+                    for (var k = 0; k < 10; k++)
+                    {
+                        nums[k] = 0;
+                    }
+                }
+                // Re-initialize the number checker array nums to 0
+                for (var k = 0; k < 10; k++)
+                {
+                    nums[k] = 0;
+                }
+            }
+            return true;
+        }
+}
