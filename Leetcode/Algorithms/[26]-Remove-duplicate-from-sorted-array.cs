d7e644f16febc7fb9979e4443ed7638e8e10b939
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-06-22 19:34:28 +0600

    Create [26]-Remove-duplicate-from-sorted-array.cs

---
+++ Leetcode/Algorithms/[26]-Remove-duplicate-from-sorted-array.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if (nums.Length==0) return 0;
+        int j = 0;
+
+        for(int i = 1; i < nums.Length; ++i) {
+            if (nums[i] != nums[j])
+            {
+                nums[++j] = nums[i];
+            }
+               
+        }
+        return j+1;
+    }
+}
