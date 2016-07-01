6c18f7927a28d17893a0c35985ac10c4e994da46
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-07-01 18:51:17 +0600

    Update [136]-Single-numbers.cs

--- Leetcode/Algorithms/[136]-Single-numbers.cs
+++ Leetcode/Algorithms/[136]-Single-numbers.cs
@@ -2,6 +2,7 @@ public class Solution {
     public int SingleNumber(int[] nums) {
         int res=0;
         for(int i=0; i<nums.Length; i++){
+            // X^X = 0, X^0 = X Where X is an integer
             res^=nums[i];
         }
         return res;
