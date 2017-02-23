3f38c4b5c5c8028da5623bf09e54a9335375aaf9
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2017-02-24 00:33:24 +0600

    Update [500]-Keyboard Row.cs

--- Leetcode/Algorithms/[500]-Keyboard Row.cs
+++ Leetcode/Algorithms/[500]-Keyboard Row.cs
@@ -1,1 +1,23 @@
-Code goes here
+public class Solution {
+    public string[] FindWords(string[] words) {
+        
+            string lower = "zxcvbnm";
+            string middle = "asdfghjkl";
+            string upper = "qwertyuiop";
+
+            List<string> ansList = new List<string>();
+
+
+            foreach (var word in words)
+            {
+                var wordSmall = word.ToLower();
+                bool isFound = wordSmall.All(x => lower.Contains(x)) || wordSmall.All(x => middle.Contains(x)) || wordSmall.All(x => upper.Contains(x));
+                if (isFound)
+                {
+                    ansList.Add(word);
+                }
+
+            }
+            return ansList.ToArray();
+    }
+}
